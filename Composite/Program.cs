2703b694046ebbe67ba2a579c72151c843aabd1e
Ignatius Chandra Kurniawan <ignatius.c.k@gmail.com>
2023-05-09 11:47:50 +0700

    update

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -1,27 +1,30 @@
-﻿class Program
+﻿static class Program
 {
     public static void Main(string[] args)
     {
-        Product product_1 = new() { Name = "Fish", Price = 5000 };
-        Product product_2 = new() { Name = "Apple", Price = 1500 };
-        Product product_3 = new() { Name = "Vegetable", Price = 2000 };
+        Product Fish = new() { Name = "Fish", Price = 5000 };
+        Product Apple = new() { Name = "Apple", Price = 1500 };
+        Product Vegetable = new() { Name = "Vegetable", Price = 2000 };
 
         Box Box_1 = new() { BoxName = "Fish Box" };
-        Box_1.Add(product_1);
-        Box_1.Add(product_1);
-        Box_1.Add(product_1);
+        Box_1.AddProduct(Fish);
+        Box_1.AddProduct(Fish);
+        Box_1.AddProduct(Fish);
 
         Box Box_2 = new() { BoxName = "Apple Box" };
-        Box_2.Add(product_2);
-        Box_2.Add(product_2);
+        Box_2.AddProduct(Apple);
+        Box_2.AddProduct(Apple);
 
         Box PackingBox = new() { BoxName = "Packing" };
-        PackingBox.Add(Box_1);
-        PackingBox.Add(Box_2);
-        PackingBox.Add(product_3);
+        PackingBox.AddProduct(Box_1);
+        PackingBox.AddProduct(Box_2);
+        PackingBox.AddProduct(Vegetable);
 
-        Console.WriteLine($"Apple Price: {Box_2.TotalPrice()}");
-        Console.WriteLine($"Fish Price: {Box_1.TotalPrice()}");
+        Console.WriteLine($"Fish box Price: {Box_1.TotalPrice()}");
+        Console.WriteLine($"Apple box Price: {Box_2.TotalPrice()}");
+        Console.WriteLine($"Total Price: {PackingBox.TotalPrice()}");
+
+        PackingBox.RemoveProduct(Box_1);
         Console.WriteLine($"Total Price: {PackingBox.TotalPrice()}");
     }
 
@@ -32,13 +35,12 @@
     }
 
 
-
-
     class Product : IProduct
     {
         public string? Name { get; set; }
         public decimal Price { get; set; }
 
+        //execute
         public decimal TotalPrice()
         {
             return Price;
@@ -50,19 +52,22 @@
     class Box : IProduct
     {
         private decimal _totalPrice;
-        public string BoxName { get; set; }
+        public string? BoxName { get; set; }
         public List<IProduct> List = new();
 
-        public void Add(IProduct product)
+        //add
+        public void AddProduct(IProduct product)
         {
             List.Add(product);
         }
 
-        public void Remove(IProduct product)
+        //remove
+        public void RemoveProduct(IProduct product)
         {
             List.Remove(product);
         }
 
+        //execute
         public decimal TotalPrice()
         {
             _totalPrice = 0;
